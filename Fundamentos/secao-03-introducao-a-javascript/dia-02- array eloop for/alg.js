04824bec5dd54e36461c1a802d8eef4d17dfa009
markoclimako <mkclimako@gmail.com>
2023-03-30 15:54:50 -0300

    exercicios fudamentos 3.2

---
+++ Fundamentos/secao-03-introducao-a-javascript/dia-02- array eloop for/alg.js
@@ -0,0 +1,20 @@
+// let fruits = [3, 4, 10, 1, 12];
+// let totalSum = 0;
+
+// for (let index = 0; index < fruits.length; index += 1) {
+//   totalSum += fruits[index];
+// }
+
+// if (totalSum > 15) {
+//   console.log(totalSum);
+// } else {
+//   console.log('Valor menor que 16');
+// }
+
+let n = 9;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
